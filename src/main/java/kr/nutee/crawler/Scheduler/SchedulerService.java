8a0d7b1cb7fb100c0980e4f836091fdac1f90f89
ysn <snyoun1032@gmail.com>
2021-03-02 19:12:13 +0900

    test

---
+++ src/main/java/kr/nutee/crawler/Scheduler/SchedulerService.java
@@ -0,0 +1,29 @@
+package kr.nutee.crawler.Scheduler;
+
+import kr.nutee.crawler.Enum.SchoolUrl;
+import kr.nutee.crawler.Service.NoticeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SchedulerService {
+    @Autowired
+    NoticeService noticeService;
+
+    @Scheduled(cron = "0 * * * * *")
+    public void checkPage() throws IOException {
+        noticeService.checkNotice(SchoolUrl.HAKSA_URL.getUrl(), "학사");
+        noticeService.checkNotice(SchoolUrl.SOOUP_URL.getUrl(), "수업");
+        noticeService.checkNotice(SchoolUrl.HAKJUM_URL.getUrl(), "학점");
+        noticeService.checkNotice(SchoolUrl.JANGHAK_URL.getUrl(), "장학");
+        noticeService.checkNotice(SchoolUrl.ILBAN_URL.getUrl(), "일반");
+        noticeService.checkNotice(SchoolUrl.HANGSA_URL.getUrl(), "행사");
+    }
+}
--- src/main/java/kr/nutee/crawler/Service/NoticeService.java
+++ src/main/java/kr/nutee/crawler/Service/NoticeService.java
@@ -1,14 +1,12 @@
 package kr.nutee.crawler.Service;
 
 import java.util.stream.Collectors;
-import kr.nutee.crawler.Enum.SchoolUrl;
 import kr.nutee.crawler.domain.entity.Notice;
 import kr.nutee.crawler.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -17,13 +15,13 @@ import java.util.*;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class SchedulerService {
+public class NoticeService {
 
     private final NoticeRepository noticeRepository;
     private final int TABLE_COLUMN_SIZE = 6;
 
 
-    public void getPage(String url, String catecory) throws IOException {
+    public void checkNotice(String url, String catecory) throws IOException {
         Document doc = Jsoup.connect(url).get();
         int size = doc.getElementsByTag("td").size();
         List<Notice> list = new ArrayList<>();
@@ -44,36 +42,6 @@ public class SchedulerService {
         List<Notice> notices = filterNewNotice(list);
         //sendAlarm(notices);
         addNewNotice(notices);
-    }
-
-    @Scheduled(cron = "0 * * * * *")
-    public void getHaksaPage() throws IOException {
-       getPage(SchoolUrl.HAKSA_URL.getUrl(), "학사");
-    }
-
-    @Scheduled(cron = "0 * * * * *")
-    public void getSooupPage() throws IOException {
-        getPage(SchoolUrl.SOOUP_URL.getUrl(), "수업");
-    }
-
-    @Scheduled(cron = "0 * * * * *")
-    public void getHakjumPage() throws IOException {
-        getPage(SchoolUrl.HAKJUM_URL.getUrl(), "학점");
-    }
-
-    @Scheduled(cron = "0 * * * * *")
-    public void getJanghakPage() throws IOException {
-        getPage(SchoolUrl.JANGHAK_URL.getUrl(), "장학");
-    }
-
-    @Scheduled(cron = "0 * * * * *")
-    public void getIlbanPage() throws IOException {
-        getPage(SchoolUrl.ILBAN_URL.getUrl(), "일반");
-    }
-
-    @Scheduled(cron = "0 * * * * *")
-    public void getHangsaPage() throws IOException {
-        getPage(SchoolUrl.HANGSA_URL.getUrl(), "행사");
     }
 
     private List<Notice> filterNewNotice(List<Notice> pageNotices) {
