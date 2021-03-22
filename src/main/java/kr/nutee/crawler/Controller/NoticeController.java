0649a74157abaf6654d04736886b25b847d03edb
ysn <snyoun1032@gmail.com>
2021-03-22 17:49:53 +0900

    test

--- src/main/java/kr/nutee/crawler/Controller/NoticeController.java
+++ src/main/java/kr/nutee/crawler/Controller/NoticeController.java
@@ -2,29 +2,56 @@ package kr.nutee.crawler.Controller;
 
 import kr.nutee.crawler.Service.NoticeService;
 import kr.nutee.crawler.domain.entity.Notice;
+import kr.nutee.crawler.dto.Resource.ResponseResource;
+import kr.nutee.crawler.dto.Response.NoticeData;
+import kr.nutee.crawler.dto.Response.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/notice")
+@RequestMapping(path = "/notice")//,consumes = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 public class NoticeController {
     @Autowired
     NoticeService noticeService;
 
-    @GetMapping(path = "/all")
-    public List<Notice> allNotice() {
-        return noticeService.searchAllNotice();
-    }
+//    @GetMapping(path = "/all")
+//    public List<Notice> allNotice() {
+//        return noticeService.searchAllNotice();
+//    }
 
     @GetMapping(path = "/{category}")
     public List<Notice> categoryNotice(@PathVariable("category") String category) {
         return noticeService.searchByCategory(category);
+    }
+
+    //모두
+    @GetMapping(path = "/all")
+    public ResponseEntity<ResponseResource> allNotice() {
+        List<Notice> list = noticeService.searchAllNotice();
+        List<NoticeData> listData = new ArrayList<>();
+
+        for(int i = 0; i<list.size(); i++) {
+            NoticeData noticeData = new NoticeData(list.get(i));
+            listData.add(noticeData);
+        }
+        Response response = Response.builder()
+                .code(10)
+                .message("test")
+                .body(listData)
+                .build();
+        ResponseResource resource = new ResponseResource(response,NoticeController.class);
+        return ResponseEntity.ok().body(resource);
     }
 }
---
+++ src/main/java/kr/nutee/crawler/dto/Response/NoticeData.java
@@ -0,0 +1,38 @@
+package kr.nutee.crawler.dto.Response;
+
+import kr.nutee.crawler.domain.entity.Notice;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NoticeData {
+
+    private String no;
+
+    private String title;
+
+    private String author;
+
+    private String href;
+
+    private String date;
+
+    private int hit;
+
+    private String category;
+
+    public NoticeData(Notice notice) {
+        this.no = notice.getNo();
+        this.title = notice.getTitle();
+        this.author = notice.getAuthor();
+        this.href = notice.getHref();
+        this.date = notice.getDate();
+        this.hit = notice.getHit();
+        this.category = notice.getCategory();
+    }
+}
