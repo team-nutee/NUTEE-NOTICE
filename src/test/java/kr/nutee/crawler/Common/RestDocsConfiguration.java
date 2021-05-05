4b975e4f45e6a9b0a7dafedfc685b63db90fd688
ysn <snyoun1032@gmail.com>
2021-05-05 18:32:29 +0900

    test

--- src/test/java/kr/nutee/crawler/Common/RestDocsConfiguration.java
+++ src/test/java/kr/nutee/crawler/Common/RestDocsConfiguration.java
@@ -1,19 +1,19 @@
-package kr.nutee.crawler.Common;
-
-import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-
-@TestConfiguration
-public class RestDocsConfiguration {
-
-    @Bean
-    public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcConfigurationCustomizer(){
-        return v -> v.operationPreprocessors()
-                .withRequestDefaults(prettyPrint())
-                .withResponseDefaults(prettyPrint());
-    }
-
-}
+//package kr.nutee.crawler.Common;
+//
+//import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+//import org.springframework.boot.test.context.TestConfiguration;
+//import org.springframework.context.annotation.Bean;
+//
+//import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+//
+//@TestConfiguration
+//public class RestDocsConfiguration {
+//
+//    @Bean
+//    public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcConfigurationCustomizer(){
+//        return v -> v.operationPreprocessors()
+//                .withRequestDefaults(prettyPrint())
+//                .withResponseDefaults(prettyPrint());
+//    }
+//
+//}
